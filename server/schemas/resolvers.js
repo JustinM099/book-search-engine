215f0aaf223d01deb0054f6fc8c1a798a2bb6add
JustinM099 <justinm099@mac.com>
2022-02-21 09:48:48 -0800

    finishing(?) resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,21 +1,60 @@
 const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express')
 const { signToken, authMiddleware } = require('../utils/auth');
 
-//TOKENS?!?!?
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return User.findOne({})
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const data = await User.findOne({_id: context.user_id})
+        return data
+      }
+      throw new AuthenticationError('You seem to be not logged in.');
     },
   },
   Mutation: {
     createUser: async (parent, args) => {
       const user = await User.create(args)
-      return user;
+      const token = signToken(user)
+      return { token, user }
     },
-    login: async (parent, args) => {
-        const user = await User.findOne(args)
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({  email })
+      if (!user) {
+        throw new AuthenticationError('Sorry, I could not find your login info in my database.')
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Sorry, your password seems to be incorrect.')
+      }
+      const token = signToken(user)
+      return { token, user }
+    },
+    saveBook: async (parent, { book }, context) =>  {
+      if(context.user){
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+        { $push: { savedBooks: book } },
+        { new: true }
+        )
+        return user
+      } else {
+        throw new AuthenticationError('Sorry, something went wrong.')
+      }
+
+    },
+    deleteBook: async (parent, { book }, context) => {
+      if(context.user){
+        const user = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: book } },
+          { new: true }
+        )
+        return user
+      } else {
+        throw new AuthenticationError('Sorry, something went wrong.')
+      }
     }
   },
 };
